0e55be35d558d44bd167c348d6d60e0ef8a2dfd8
lucasresa <lukkitas.1597@gmail.com>
2018-09-10 00:10:07 -0300

    Creada la estructura basica del DTB y archivo de config levantado

--- process/S-AFA/S-AFA.c
+++ process/S-AFA/S-AFA.c
@@ -1,8 +1,59 @@
-/*
- * S-AFA.c
- *
- *  Created on: 2 sep. 2018
- *      Author: utnso
- */
+#include "S-AFA.h"
+
+
+int main(){
+
+	//Creo las colas del S-AFA
+
+	cola_new = queue_create();
+	cola_ready = queue_create();
+	cola_block = queue_create();
+	cola_exec = queue_create();
+	cola_exit = queue_create();
+
+	//Levanto archivo de configuracion del S-AFA
+
+	file=config_create("CONFIG_S-AFA.cfg");
+
+	config.algoritmo=malloc(sizeof(char)*6);
+
+	config.port=config_get_int_value(file,"PUERTO");
+	config.algoritmo=config_get_string_value(file,"ALGORITMO");
+	config.quantum=config_get_int_value(file,"QUANTUM");
+	config.multiprog=config_get_int_value(file,"MULTIPROGRAMACION");
+	config.retardo=config_get_int_value(file,"RETARDO_PLANIF");
+
+	config_destroy(file);
+
+	//-------------------------------------------------------------------------------------------------------
+
+	//Aca habria que hacer la parte de la conexion con los demas procesos usando sockets. (Usando la shared library de sockets
+
+
+
+	//-------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+}
+
+//Creo el dtb y lo agrego a la cola de New
+void agregarDTBANew(char*path){
+
+
+	t_DTB dtb;
+
+	dtb.escriptorio=string_duplicate(path);
+	dtb.f_inicializacion=0;
+	dtb.pc=0;
+	dtb.id=cont_id;
+	cont_id++;
+
+	queue_push(cola_new,(void*)dtb);
+
+}
 
 
--- process/S-AFA/S-AFA.h
+++ process/S-AFA/S-AFA.h
@@ -1,13 +1,39 @@
-/*
- * S-AFA.h
- *
- *  Created on: 2 sep. 2018
- *      Author: utnso
- */
+#include "../../utils/bibliotecaDeSockets.h"
+#include <pthread.h>
+#include <commons/collections/queue.h>
+#include <commons/config.h>
 
 #ifndef S_AFA_H_
 #define S_AFA_H_
 
+typedef struct{
+
+	int id;
+	char* escriptorio;
+	int pc;
+	int f_inicializacion;
+	char** archivos;
+
+}t_DTB;
+
+typedef struct{
+
+	int port;
+	char* algoritmo;
+	int quantum;
+	int multiprog;
+	int retardo;
+
+}configuracion;
+
+t_queue* cola_new;
+t_queue* cola_ready;
+t_queue* cola_block;
+t_queue* cola_exec;
+t_queue* cola_exit;
+t_config* file;
+configuracion config;
+int cont_id=0;
 
 
 #endif /* S_AFA_H_ */
