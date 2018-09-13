06ec998933f84c5bf5f6705013f798a66b214145
lautaromss <lautaro.mss@gmail.com>
2018-09-13 15:23:11 -0300

    FM9 carga su config y agrego logs al gitignore

--- process/FM9/FM9.c
+++ process/FM9/FM9.c
@@ -1,8 +1,31 @@
-/*
- * FM9.c
- *
- *  Created on: 2 sep. 2018
- *      Author: utnso
- */
+#include "FM9.h"
+
+int main(){
+	log_FM9 = log_create("FM9.log","FM9",true,LOG_LEVEL_INFO);
+
+	file_FM9=config_create("CONFIG_FM9.cfg");
+
+	config_FM9.puerto_fm9=config_get_int_value(file_FM9,"PUERTO");
+	config_FM9.modo=detectarModo(config_get_string_value(file_FM9,"MODO"));
+	config_FM9.tamanio=config_get_int_value(file_FM9,"TAMANIO");
+	config_FM9.max_linea=config_get_int_value(file_FM9,"MAX_LINEA");
+	config_FM9.tam_pagina=config_get_int_value(file_FM9,"TAM_PAGINA");
+
+	config_destroy(file_FM9);
+}
 
 
+
+t_modo detectarModo(char* modo){
+
+	t_modo modo_enum;
+
+	if(string_equals_ignore_case(modo,"TPI")){
+		modo_enum=TPI;
+	}else if(string_equals_ignore_case(modo,"SPA")){
+		modo_enum=SPA;
+	}else {
+		modo_enum=SEG;
+	}
+	return modo_enum;
+}
--- process/FM9/FM9.h
+++ process/FM9/FM9.h
@@ -1,13 +1,31 @@
-/*
- * FM9.h
- *
- *  Created on: 2 sep. 2018
- *      Author: utnso
- */
-
 #ifndef FM9_H_
 #define FM9_H_
 
+#include "../../utils/bibliotecaDeSockets.h"
+#include <pthread.h>
+#include <commons/log.h>
+#include <commons/config.h>
 
+typedef enum{
+	SEG,
+	TPI,
+	SPA
+}t_modo;
+
+typedef struct{
+
+	int puerto_fm9;
+	t_modo modo;
+	int tamanio;
+	int max_linea;
+	int tam_pagina;
+
+}t_config_FM9;
+
+t_config_FM9 config_FM9;
+t_config* file_FM9;
+t_log* log_FM9;
+
+t_modo detectarModo(char*);
 
 #endif /* FM9_H_ */
