dcc181c1d0704acfef2fa6b5449b02144a23c6b7
Angel Luis Eduardo Gomez <angelluisgo@gmail.com>
2018-09-14 01:44:22 -0300

    Defino la estructura para hacer la conexiones inciales

--- process/MDJ/MDJ.h
+++ process/MDJ/MDJ.h
@@ -8,6 +8,15 @@
 #ifndef MDJ_H_
 #define MDJ_H_
 
+#include <commons/config.h>
 
+typedef struct{
+ 	int puerto_mdj;
+	char* mount_point;
+	int time_delay;
+ }t_config_MDJ;
+
+ t_config* file_MDJ;
+ t_config_MDJ config_MDJ;
 
 #endif /* MDJ_H_ */
