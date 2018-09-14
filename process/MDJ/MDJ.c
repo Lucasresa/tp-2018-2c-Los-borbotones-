5cee1b0a9a6dc3138c492640825bfecaf99c67b6
Angel Luis Eduardo Gomez <angelluisgo@gmail.com>
2018-09-14 16:45:36 -0300

    Agregando funciones al MDJ.h

--- process/MDJ/MDJ.c
+++ process/MDJ/MDJ.c
@@ -5,4 +5,12 @@
  *      Author: utnso
  */
 
+#include "MDJ.h"
 
+void init_filesystem(t_config* config) {
+}
+
+int main(int argc, char *argv[]) {
+	init_filesystem(t_config_MDJ);
+	return 0;
+}
--- process/MDJ/MDJ.h
+++ process/MDJ/MDJ.h
@@ -8,6 +8,9 @@
 #ifndef MDJ_H_
 #define MDJ_H_
 
+#include "../../utils/bibliotecaDeSockets.h"
+#include <pthread.h>
+#include <commons/log.h>
 #include <commons/config.h>
 
 typedef struct{
@@ -18,5 +21,11 @@ typedef struct{
 
  t_config* file_MDJ;
  t_config_MDJ config_MDJ;
+ t_log* logger;
+
+void funcionHandshake(int, void*);
+void funcionRecibirPeticion(int, void*);
+
+t_modo detectarModo(char*);
 
 #endif /* MDJ_H_ */
